f519b1e9b8de746dd09a02a79c685c5cd6b3d657
Superfice <wjyi@vip.qq.com>
2021-09-07 10:57:07 +0800

    Add files via upload

--- QZupdateCookies.py
+++ QZupdateCookies.py
@@ -149,11 +149,13 @@ if __name__ == '__main__':
                     ptck = wstopt(i["value"])
                     if ptck == "error":
                         print("第%s个wskey转换失败, pin:%s" % (count, wspin))
-                    else:
-                        if update(ptck, item["_id"]):
+                qlid = item["_id"]
+                if update(ptck, qlid):
                             print("第%s个wskey更新成功, pin:%s" % (count, wspin))
                         else:
                             print("第%s个wskey更新失败, pin:%s" % (count, wspin))
+                else:
+                    print("第%s个wskey无需更新, pin:%s" % (count, wspin))
             else:
                 ptck = wstopt(i["value"])
                 if ptck == "error":
